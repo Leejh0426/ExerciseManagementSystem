832053030b0015cf732a5e8ed8514e40b9239c62
Leejh0426 <leejunho2000@gmail.com>
2022-03-10 23:06:30 +0900

    added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Exercises");
+		System.out.println("2. Delete Exercises");
+		System.out.println("3. Edit Exercises");
+		System.out.println("4. View Exercises");
+		System.out.println("5. Exit");
+	}
+}
