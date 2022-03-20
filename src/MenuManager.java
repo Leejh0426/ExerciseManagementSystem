c4472097908938afce2fa1d1a155d54ec030de7a
Leejh0426 <leejunho2000@gmail.com>
2022-03-20 17:21:16 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,11 +1,52 @@
+import java.util.Scanner;
 
 public class MenuManager {
-
 	public static void main(String[] args) {
-		System.out.println("1. Add Exercises");
-		System.out.println("2. Delete Exercises");
-		System.out.println("3. Edit Exercises");
-		System.out.println("4. View Exercises");
-		System.out.println("5. Exit");
+		int num = 5;
+		Scanner sc = new Scanner(System.in);
+		while (num != 6) {
+			System.out.println("1. Add Exercises");
+			System.out.println("2. Delete Exercises");
+			System.out.println("3. Edit Exercises");
+			System.out.println("4. View Exercises");
+			System.out.println("5. Show a Menu");
+			System.out.println("6. Exit");
+			
+			num = sc.nextInt();
+			
+			switch(num) {
+				case 1 : 
+					System.out.print("date");
+					String date = sc.next();
+					System.out.print("kind of exercise");
+					String kindofexercise = sc.next();
+					System.out.print("time(miniute)");
+					int time = sc.nextInt();
+					System.out.print("where");
+					String where = sc.next();
+					System.out.print("partner");
+					String partner = sc.next();
+					break;
+				case 2 :
+					System.out.print("date");
+					String date2 = sc.next();
+					break;
+					
+				case 3 :
+					System.out.print("date");
+					String date3 = sc.next();
+					break;
+				case 4 :
+					System.out.print("date");
+					String date4 = sc.next();
+					break;
+				case 5 : 
+					
+				case 6 :
+				
+					
+			}
+			
+		}
 	}
 }
