f508de00bb11c4a2ad8b8177452d8d4336a16b7a
Leejh0426 <leejunho2000@gmail.com>
2022-04-03 00:18:54 +0900

    +exerciseManagement,+Exercise

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,32 +2,33 @@ import java.util.Scanner;
 
 public class MenuManager {
 	public static void main(String[] args) {
-		int num = 5;
 		Scanner sc = new Scanner(System.in);
-		while (num != 6) {
+		ExerciseManager Exercisemanager = new ExerciseManager(sc);
+		int num = -1;
+
+		while (num != 5) {
 			System.out.println("1. Add Exercises");
 			System.out.println("2. Delete Exercises");
 			System.out.println("3. Edit Exercises");
 			System.out.println("4. View Exercises");
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");
+			System.out.println("5. Exit");
 			
 			num = sc.nextInt();
 			
 			if(num == 1) {
-				AddExercise();
+				Exercisemanager.AddExercise();
 			}
 			
 			else if(num == 2) {
-				DeleteExercise();
+				Exercisemanager.DeleteExercise();
 			}
 			
 			else if(num == 3) {
-				EditExercise();
+				Exercisemanager.EditExercise();
 			}
 			
 			else if(num == 4) {
-				ViewExercise();
+				Exercisemanager.ViewExercise();
 			}
 			
 			else
@@ -36,38 +37,7 @@ public class MenuManager {
 			}
 			
 	}   														//main }
-	
-	public static void AddExercise() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("date");
-		String date = sc.next();
-		System.out.print("kind of exercise");
-		String kind_of_exercise = sc.next();
-		System.out.print("time(miniute)");
-		int time = sc.nextInt();
-		System.out.print("where");
-		String where = sc.next();
-		System.out.print("partner");
-		String partner = sc.next();
-		}
-	
-	public static void DeleteExercise() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("date");
-		String date = sc.next();
-		}
-	
-	public static void EditExercise() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("date");
-		String date = sc.next();
-		}
-	
-	public static void ViewExercise() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("date");
-		String date = sc.next();
-		}
+
 				
 }														//class }
 	
